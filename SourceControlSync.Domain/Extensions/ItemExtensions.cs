7370fd0aa18799953f976cb7a1791fcfd1dfb3e1
Brian VanSchyndel <brianvans@hotmail.com>
2015-08-26 14:11:08 -0400

    Add ItemExtensions which was missing from previous commit

---
+++ SourceControlSync.Domain/Extensions/ItemExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SourceControlSync.Domain.Models;
+
+namespace SourceControlSync.Domain.Extensions
+{
+    public static class ItemExtensions
+    {
+        public static bool IsInRoot(this Item item, string root)
+        {
+            return item.Path.StartsWith(root);
+        }
+    }
+}
